763e34e46490ffd8ab7ea51ac5aa0fc18b82b9ce
Ziv Dayan <ziv987@gmail.com>
2024-04-23 12:35:54 +0300

    created all of the required files

---
+++ src/firstPass.h
@@ -0,0 +1,1 @@
+extern int firstPass(struct tranlsation_unit *prog, const char * amFileName, FILE * amFile);
---
+++ src/strcuts.h
@@ -0,0 +1,35 @@
+#define MAX_MEM_SIZE 80
+#define MAX_SYMBOLS 80
+
+struct symbol{
+    char symName[32];
+    enum {
+        symExtern,
+        symEntry,
+        symCode,
+        symData,
+        symEntryCode,
+        symEntryData
+    } symType;
+    int address;
+};
+
+struct extr {
+    char *externalName;
+    int addresses[1024];
+    int address_count;
+};
+
+struct translation_unit {
+    int code_image[MAX_MEM_SIZE];
+    int IC;
+    int data_image[MAX_MEM_SIZE];
+    int DC;
+    struct symbol symbol_table[MAX_SYMBOLS];
+    int symCount;
+    struct extr externals[MAX_SYMBOLS];
+    int extCount;
+    struct symbol * entries[MAX_SYMBOLS];
+    int entries_count;
+
+};
