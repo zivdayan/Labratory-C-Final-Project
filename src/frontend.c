ad01984cc1dc59e2c708f8518aee6d9f1075691c
Ziv Dayan <ziv987@gmail.com>
2024-04-23 12:34:29 +0300

    created all of the required files

--- src/frontend.c
+++ src/frontend.c
@@ -1,5 +1,8 @@
 #include "frontend.h"
 #include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+
 
 #define SPACES " \t\v\f"
 
@@ -47,42 +50,34 @@ struct ast get_ast_from_line(char* line)
 }
 
 
+
 struct string_sep_result {
     char *strings[80];
-    int strings_count; 
+    int strings_count;
 };
-
-static struct string_sep_result string_sep(char* str)
-{
-    int strings_count=0;
+struct string_sep_result string_sep(char * str) {
+    int strings_count = 0;
     char *s;
     struct string_sep_result ssr = {0};
     while(isspace(*str)) str++;
-    if(*str == '\0') 
-    {
+    if(*str == '\0') {
         return ssr;
     }
 
-    do
-    {
+    do {
         ssr.strings[strings_count++] = str;
-        s = strpbrk(str, SPACES);
-        if (s)
-        {
-            *s='\0';
+        s = strpbrk(str,SPACES);
+        if(s) {
+            *s = '\0';
             s++;
-            while(isspace(*s)) s++;
-            if(*str == '\0') 
+            while(isspace(*s))s++;
+            if(*s == '\0')
                 break;
-            str = s;
-        }
-        else{
+            str = s;    
+        }else {
             break;
         }
-    }
-    while(1);
-
+    }while(1);
     ssr.strings_count = strings_count;
     return ssr;
-
 }
